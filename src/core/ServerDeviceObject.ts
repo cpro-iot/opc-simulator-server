5c153511095a6115b62df011581fdfea00d3c1fb
Tq-bit <t.quante@outlook.com>
2024-06-17 10:19:00 +0200

    fix: sinus function

--- src/core/ServerDeviceObject.ts
+++ src/core/ServerDeviceObject.ts
@@ -1,120 +1,126 @@
-import { Namespace, OPCUAServer, UAFolder, DataType, UAObjectsFolder, Variant, StatusCodes, StatusCode, VariantT } from "node-opcua";
-import { DeviceFolder, DeviceNode } from "../../@types";
+import { Namespace, OPCUAServer, UAFolder, DataType, UAObjectsFolder, Variant, StatusCodes, StatusCode, VariantT } from 'node-opcua';
+import { DeviceFolder, DeviceNode } from '../../@types';
 
 export default class ServerDeviceObject {
-  private device: DeviceFolder;
-  private namespace: Namespace;
-  private objectsFolder: UAObjectsFolder;
+    private device: DeviceFolder;
+    private namespace: Namespace;
+    private objectsFolder: UAObjectsFolder;
 
-  constructor(server: OPCUAServer, device: DeviceFolder) {
-    this.device = device;
-    this.namespace = server.engine.addressSpace?.getOwnNamespace() as Namespace;
-    this.objectsFolder = server.engine.addressSpace?.rootFolder.objects as UAObjectsFolder;
-  }
-
-  public init() {
-    const deviceRootFolder = this.registerDeviceFolderNode(this.objectsFolder, this.device);
-    this.device.items.forEach((node: DeviceFolder | DeviceNode) => {
-      this.registerDeviceNode(deviceRootFolder, node);
-    });
-  }
-
-  private registerDeviceNode(owner: UAFolder, node: DeviceFolder | DeviceNode) {
-    const isFolder = node.hasOwnProperty('items');
-    if (isFolder) {
-      const deviceSubFolder = this.registerDeviceFolderNode(owner, node as DeviceFolder);
-      (node as DeviceFolder).items.forEach((item) => this.registerDeviceNode(deviceSubFolder, item));
-    } else  {
-      this.registerDeviceVariableNode(owner, node as DeviceNode);
-    }
-  }
-
-  private registerDeviceFolderNode(owner: UAFolder, folder: DeviceFolder) {
-    console.log(`Assembling folder: ${folder.name}`);
-    return this.namespace.addFolder(owner, { browseName: folder.name });
-  }
-
-  private registerDeviceVariableNode(owner: UAFolder, node: DeviceNode) {
-    console.log(`Assembling node: ${node.name}`);
-    let _variableValue = node.value;
-
-    if (node.simulation) {
-      console.log(`Enabling '${node.simulation.type}' simulation for '${node.name}'`);
-      this.simulateNodeValue(node);
+    constructor(server: OPCUAServer, device: DeviceFolder) {
+        this.device = device;
+        this.namespace = server.engine.addressSpace?.getOwnNamespace() as Namespace;
+        this.objectsFolder = server.engine.addressSpace?.rootFolder.objects as UAObjectsFolder;
     }
 
-    return this.namespace.addVariable({
-      componentOf: owner,
-      nodeId: node.id,
-      browseName: node.name,
-      dataType: this.inferValueType(node.value),
-      minimumSamplingInterval: 250,
-      value: this.constructNodeVariant(node, _variableValue),
-    })
-  }
-
-  private constructNodeVariant(node: DeviceNode, value: string | number | boolean) {
-    let _value: { get: () => Variant | StatusCode; set: (variant: VariantT<number | boolean | string, DataType>) => Variant | StatusCode } = {
-      get: () => StatusCodes.BadNotReadable,
-      set: () => StatusCodes.BadNotWritable,
-    };
-
-    if (node.valueMethods.includes('get')) {
-      _value.get = () => new Variant({ dataType: this.inferValueType(node.value), value: node.value });
+    public init() {
+        const deviceRootFolder = this.registerDeviceFolderNode(this.objectsFolder, this.device);
+        this.device.items.forEach((node: DeviceFolder | DeviceNode) => {
+            this.registerDeviceNode(deviceRootFolder, node);
+        });
     }
 
-    if (node.valueMethods.includes('set')) {
-      _value.set = (variant: VariantT<number | boolean | string, DataType>) => {
-        node.value = variant.value;
-        return StatusCodes.Good;
-      };
+    private registerDeviceNode(owner: UAFolder, node: DeviceFolder | DeviceNode) {
+        const isFolder = node.hasOwnProperty('items');
+        if (isFolder) {
+            const deviceSubFolder = this.registerDeviceFolderNode(owner, node as DeviceFolder);
+            (node as DeviceFolder).items.forEach((item) => this.registerDeviceNode(deviceSubFolder, item));
+        } else {
+            this.registerDeviceVariableNode(owner, node as DeviceNode);
+        }
     }
 
-    return _value;
-  }
-
-  private simulateNodeValue(node: DeviceNode) {
-    const { type, value, interval } = node.simulation || {};
-    if (type === 'increment') {
-      typeof node.value === "number" ?
-        setInterval(() => ((node.value as number) += value || 1), (interval || 1) * 1000) : console.error("Can't increment non number value");
+    private registerDeviceFolderNode(owner: UAFolder, folder: DeviceFolder) {
+        console.log(`Assembling folder: ${folder.name}`);
+        return this.namespace.addFolder(owner, { browseName: folder.name });
     }
 
-    if (type === 'decrement') {
-      typeof node.value === "number" ?
-        setInterval(() => ((node.value as number) -= value || 1), (interval || 1) * 1000) : console.error("Can't decrement non number value");
+    private registerDeviceVariableNode(owner: UAFolder, node: DeviceNode) {
+        console.log(`Assembling node: ${node.name}`);
+        let _variableValue = node.value;
+
+        if (node.simulation) {
+            console.log(`Enabling '${node.simulation.type}' simulation for '${node.name}'`);
+            this.simulateNodeValue(node);
+        }
+
+        return this.namespace.addVariable({
+            componentOf: owner,
+            nodeId: node.id,
+            browseName: node.name,
+            dataType: this.inferValueType(node.value),
+            minimumSamplingInterval: 250,
+            value: this.constructNodeVariant(node, _variableValue),
+        });
     }
 
-    if (type === 'randomize') {
-      typeof node.value === "number" ?
-        setInterval(
-          () => {
-            const min = Math.random() * (node.simulation?.randomize?.max || 1);
-            const max = Math.random() * (node.simulation?.randomize?.min || 1);
-            node.value = +((node.value as number) + min - max).toFixed(2);
-          },
-          (interval || 1) * 1000,
-        ) : console.error("Can't randomize non number value");
+    private constructNodeVariant(node: DeviceNode, value: string | number | boolean) {
+        let _value: { get: () => Variant | StatusCode; set: (variant: VariantT<number | boolean | string, DataType>) => Variant | StatusCode } = {
+            get: () => StatusCodes.BadNotReadable,
+            set: () => StatusCodes.BadNotWritable,
+        };
+
+        if (node.valueMethods.includes('get')) {
+            _value.get = () => new Variant({ dataType: this.inferValueType(node.value), value: node.value });
+        }
+
+        if (node.valueMethods.includes('set')) {
+            _value.set = (variant: VariantT<number | boolean | string, DataType>) => {
+                node.value = variant.value;
+                return StatusCodes.Good;
+            };
+        }
+
+        return _value;
     }
 
-    if(type === 'sinus') {
-      let t = 1;
-      typeof node.value === "number" ?
-        setInterval(
-          () => {
-            node.value = +(node.simulation?.sinus?.amplitude || 1) * (Math.sin(t / 50) + (node.simulation?.sinus?.offset || 0));
-            t ++;
-          },
-          (interval || 1) * 1000,
-        ) : console.error("Can't sinus non number value");
+    private simulateNodeValue(node: DeviceNode) {
+        const { type, value, interval } = node.simulation || {};
+        if (type === 'increment') {
+            typeof node.value === 'number'
+                ? setInterval(() => ((node.value as number) += value || 1), (interval || 1) * 1000)
+                : console.error("Can't increment non number value");
+        }
+
+        if (type === 'decrement') {
+            typeof node.value === 'number'
+                ? setInterval(() => ((node.value as number) -= value || 1), (interval || 1) * 1000)
+                : console.error("Can't decrement non number value");
+        }
+
+        if (type === 'randomize') {
+            typeof node.value === 'number'
+                ? setInterval(
+                      () => {
+                          const min = Math.random() * (node.simulation?.randomize?.max || 1);
+                          const max = Math.random() * (node.simulation?.randomize?.min || 1);
+                          node.value = +((node.value as number) + min - max).toFixed(2);
+                      },
+                      (interval || 1) * 1000,
+                  )
+                : console.error("Can't randomize non number value");
+        }
+
+        if (type === 'sinus') {
+            let t = 1;
+            typeof node.value === 'number'
+                ? setInterval(
+                      () => {
+                          const sinus = +(node.simulation?.sinus?.amplitude || 1) * Math.sin(t / 50);
+                          const offset = +(node.simulation?.sinus?.offset || 0);
+                          node.value = +(sinus + offset).toFixed(2);
+                          if (t++ > 32767) t = 1;
+                      },
+                      (interval || 1) * 1000,
+                  )
+                : console.error("Can't sinus non number value");
+        }
     }
-  }
 
-  private inferValueType(value: string | number | boolean) {
-    if (typeof value === "string") return DataType.String;
-    if (typeof value === "number") return DataType.Double;
-    if (typeof value === "boolean") return DataType.Boolean;
+    private inferValueType(value: string | number | boolean) {
+        if (typeof value === 'string') return DataType.String;
+        if (typeof value === 'number') return DataType.Double;
+        if (typeof value === 'boolean') return DataType.Boolean;
 
-    return DataType.Null;
-  }
+        return DataType.Null;
+    }
 }
